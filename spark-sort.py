cb3dabd2821c26079b07cdbd59e97c4e191ea0fa
yuanchun <immr.shen@gmail.com>
2021-02-08 14:18:19 +0800

    can sort records less than 100,000 items

--- spark-sort.py
+++ spark-sort.py
@@ -9,22 +9,22 @@ app_name = "spark"
 conf = SparkConf().setAppName(app_name)
 sc = SparkContext()
 
-input_file = sc.binaryFiles(input_bucket+input_path)
+input_file = sc.binaryRecords(input_bucket+input_path, 100)
+print("len first: {}".format(len(input_file.first())))
 print("input count: " + str(input_file.count()))
-# input_file.map(lambda content : content[i: i+100] for i in range(1000000))
-# input_file = input_file.flatMap(lambda line: line.split("\n"))
-# print("input count: " + str(input_file.count()))
-# records = []
-# with open("unsorted.dat", "rb") as f:
-#   r = f.read(100)
-#   while len(r) == 100:
-#     records.append(r)
-#     r = f.read(100)
 
-sorted_op = input_file.flatMap(lambda line: line.split(10))\
-    .map(lambda line: (line[:10], line[10:100]))\
+sorted_op = input_file.map(lambda line: (line[:10], line[10:100]))\
     .sortByKey()\
-    .map(lambda key, value: key+value+" ")
+    .map(lambda item: item[0]+item[1])
 
-sorted_op.saveAsTextFile("sorted.data")
+result = sorted_op.collect()
+print("type(result): {}".format(type(result)))
+print("len(result): {}".format(len(result)))
+print("type(result[0]): {}".format(type(result[0])))
+print("len(result[0]): {}".format(len(result[0])))
+
+with open("sorted.dat", "ab") as result_file:
+    for output in result:
+        result_file.write(output)
+# sorted_op.saveAsSequenceFile("sorted.dat")
 # sortBy(lambda r: r[0:10])
