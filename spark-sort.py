37ab941a1cae2be549d765bfc70f36427d5ae721
yuanchun <immr.shen@gmail.com>
2021-02-10 03:27:59 +0800

    add s3 access method from local, and add partition

--- spark-sort.py
+++ spark-sort.py
@@ -1,22 +1,43 @@
 import time
+import os
+import configparser
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import functions as F
 
 input_bucket = ""
 input_path = "unsorted.dat"
+# input_bucket = "s3://unsorted-bucket/"
+# input_path = "unsorted.dat"
 
 app_name = "spark"
 
 conf = SparkConf().setAppName(app_name)
 sc = SparkContext()
 
+# aws_profile = 'default'
+# config = configparser.ConfigParser()
+# config.read(os.path.expanduser("~/.aws/credentials"))
+# access_id = config.get(aws_profile, "aws_access_key_id") 
+# access_key = config.get(aws_profile, "aws_secret_access_key")
+# hadoop_conf = sc._jsc.hadoopConfiguration()
+# hadoop_conf.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+# hadoop_conf.set("fs.s3n.awsAccessKeyId", access_id)
+# hadoop_conf.set("fs.s3n.awsSecretAccessKey", access_key)
+
 input_file = sc.binaryRecords(input_bucket+input_path, 100)
+input_file = input_file.map(lambda line: (line[:10], line[10:100]))
+input_file = input_file.partitionBy(10)
+
+print("input file patition num: {}".format(len(input_file.glom().collect())))
 print("len first: {}".format(len(input_file.first())))
+print("type first: {}".format(type(input_file.first())))
+print("len first[0]: {}".format(len(input_file.first()[0])))
+print("type first[0]: {}".format(type(input_file.first()[0])))
 print("input count: " + str(input_file.count()))
 
 start = time.time()
-sorted_op = input_file.map(lambda line: (line[:10], line[10:100]))\
-    .sortByKey()\
+# input_file.map(lambda line: (line[:10], line[10:100]))\
+sorted_op = input_file.sortByKey()\
     .map(lambda item: item[0]+item[1])
 result = sorted_op.collect()
 end = time.time()
