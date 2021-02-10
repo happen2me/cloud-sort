a3e47ee89832fc7ab1e589af1f3bb21feaf7d87f
yuanchun <immr.shen@gmail.com>
2021-02-10 08:41:37 +0800

    updated upload method

--- spark-sort.py
+++ spark-sort.py
@@ -1,9 +1,33 @@
 import time
 import os
 import configparser
+import logging
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import functions as F
 import boto3
+from botocore.exceptions import ClientError
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 input_bucket = "s3://unsorted-bucket/"
 input_path = "unsorted-mini.dat"
@@ -32,12 +56,12 @@ input_file = sc.binaryRecords(input_bucket+input_path, 100)
 input_file = input_file.map(lambda line: (line[:10], line[10:100]))
 input_file = input_file.partitionBy(10)
 
-print("input file patition num: {}".format(len(input_file.glom().collect())))
-print("len first: {}".format(len(input_file.first())))
-print("type first: {}".format(type(input_file.first())))
-print("len first[0]: {}".format(len(input_file.first()[0])))
-print("type first[0]: {}".format(type(input_file.first()[0])))
-print("input count: " + str(input_file.count()))
+logging.info("input file patition num: {}".format(len(input_file.glom().collect())))
+logging.debug("len first: {}".format(len(input_file.first())))
+logging.debug("type first: {}".format(type(input_file.first())))
+logging.debug("len first[0]: {}".format(len(input_file.first()[0])))
+logging.debug("type first[0]: {}".format(type(input_file.first()[0])))
+logging.info("input count: " + str(input_file.count()))
 
 start = time.time()
 # input_file.map(lambda line: (line[:10], line[10:100]))\
@@ -45,12 +69,12 @@ sorted_op = input_file.sortByKey()\
     .map(lambda item: item[0]+item[1])
 result = sorted_op.collect()
 end = time.time()
-print("Time elapse in seconds: {}".format(end - start))
+logging.info("Time elapse in seconds: {}".format(end - start))
 
-print("type(result): {}".format(type(result)))
-print("len(result): {}".format(len(result)))
-print("type(result[0]): {}".format(type(result[0])))
-print("len(result[0]): {}".format(len(result[0])))
+logging.debug("type(result): {}".format(type(result)))
+logging.debug("len(result): {}".format(len(result)))
+logging.debug("type(result[0]): {}".format(type(result[0])))
+logging.debug("len(result[0]): {}".format(len(result[0])))
 
 
 with open(output_path, "wb") as result_file:
@@ -58,9 +82,10 @@ with open(output_path, "wb") as result_file:
         result_file.write(output)
 
 # write to s3
-with open(output_path, 'rb') as data
-    s3 = boto3.resource('s3')
-    s3.Bucket(output_bucket_name).put_object(Key=output_path, Body=data)
+# with open(output_path, 'rb') as data:
+#     s3 = boto3.resource('s3')
+#     s3.Bucket(output_bucket_name).put_object(Key=output_path, Body=data)
+upload_file(output_path, output_bucket_name)
 
 # sorted_op.saveAsSequenceFile("sorted.dat")
 # sortBy(lambda r: r[0:10])
